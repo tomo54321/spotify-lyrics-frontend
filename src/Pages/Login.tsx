f61e0f086f9e6a1f52744d2e55cfdd9ea2f3a379
Tom Reeve <tom@graphitas.co.uk>
2021-08-02 19:50:25 +0100

    Fix login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -6,7 +6,7 @@ export const Login = () => {
             <h1>Lyrics for Spotify</h1>
             <p className="subtitle">Get lyrics that you can sing along to. Begin playing something in Spotify and get realtime lyrics.</p>
             <p className="sublogin">All you need to do is login using the button below!</p>
-            <a href="http://localhost:8000/auth/spotify" className="spotify-login">Login with Spotify</a>        
+            <a href={`${process.env.REACT_APP_API}/auth/spotify`} className="spotify-login">Login with Spotify</a>        
         </div>
     )
 
